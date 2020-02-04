15dcdf8a06e2f83d9ee96b688143e84c064657a9
GLXL <60291185+GLXL@users.noreply.github.com>
2020-02-04 15:29:09 +0000

    Midnight man testing
    h

---
+++ The_Midnight_Game/Assets/Scripts/MidnightMan/CurrentRoom_Morgan.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentRoom_Morgan : MonoBehaviour
+{
+    public MMTargeting_Morgan targetScript;
+    //set name of room manually
+    //doesnt matter, as long as no 2 rooms are named the same
+    public string roomName;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            targetScript.playerRoom = roomName;
+        }
+        else if (other.tag == "midnightMan")
+        {
+            targetScript.midnightManRoom = roomName;
+        }
+    }
+}
---
+++ The_Midnight_Game/Assets/Scripts/MidnightMan/MMTargeting_Morgan.cs
@@ -0,0 +1,51 @@
+﻿// Morgan Pryor : 03/02/2020 16:54
+///
+/// This script works out how close to the player the midnight man is and checks if the midnight man is in the same room
+///
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MMTargeting_Morgan : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject midnightMan;
+
+    //current room for player and midnight man
+    //aquired in currentRoom script
+    internal string playerRoom;
+    internal string midnightManRoom;
+
+    const int highNumber = 1000;
+
+    //I need the player and the midnightmans exact position whenever the player and the midnight man are in the same room, It would be wasteful to consider the Y pos in calculations
+    Vector2 differenceVector;
+    internal float distanceToPlayer;
+    
+    //true if player room = midnightman room
+    bool isWithPlayer = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isWithPlayer)
+        {
+            //calculate distance to player
+            differenceVector = new Vector2
+                ((midnightMan.transform.position.x - player.transform.position.x),
+                (midnightMan.transform.position.z - player.transform.position.z));
+            //pythag giving distance from midnightMan to player
+            distanceToPlayer = Mathf.Sqrt((differenceVector.x * differenceVector.x) + (differenceVector.y * differenceVector.y));
+        }
+        else if (distanceToPlayer != highNumber)
+        {
+            //if not in same room as player, distance to player is set to a arbitrary high number
+            distanceToPlayer = highNumber;
+        }
+    }
+}
