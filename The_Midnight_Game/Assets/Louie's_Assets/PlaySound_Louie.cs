31ee0abf8d90fb8f38ead750cf29ac6aa18d3acf
LouieWilliamson <louie-here@hotmail.com>
2020-02-05 13:29:14 +0000

    Added Whisper and Ambient Audio

---
+++ The_Midnight_Game/Assets/Louie's_Assets/PlaySound_Louie.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound_Louie : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audio;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        PlaySound();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        PauseAudio();
+    }
+    void PlaySound()
+    {
+        audio.Play();
+    }
+    void PauseAudio()
+    {
+        audio.Pause();
+    }
+}
