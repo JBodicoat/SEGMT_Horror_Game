8ed21b832e954c8c3f759d900eeff48e0b274be8
LouieWilliamson <louie-here@hotmail.com>
2020-02-10 17:21:50 +0000

    Implemented Simple Journal

---
+++ The_Midnight_Game/Assets/Louie's_Assets/Scripts/JournalManager_Louie.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JournalManager_Louie : MonoBehaviour
+{
+    // Start is called before the first frame update
+	private bool isJournalOn;
+    private CanvasGroup journal;
+
+    void Start()
+    {
+		//handle text 
+		isJournalOn = false;
+        journal = GameObject.Find("Journal").GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            toggleJournal();
+        }
+    }
+
+	void toggleJournal()
+	{
+
+		if (isJournalOn)
+		{
+            journal.alpha = 0;
+            isJournalOn = false;
+        }
+        else 
+		{
+            journal.alpha = 0.8f;
+            isJournalOn = true;
+		}
+	}
+}
