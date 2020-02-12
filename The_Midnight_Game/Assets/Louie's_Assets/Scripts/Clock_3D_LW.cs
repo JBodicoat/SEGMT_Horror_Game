a8a5f61342601a6e17351031eab745fbbbee0ce5
LouieWilliamson <louie-here@hotmail.com>
2020-02-12 20:27:20 +0000

    Pocket Watch Tests

---
+++ The_Midnight_Game/Assets/Louie's_Assets/Scripts/Clock_3D_LW.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock_3D_LW : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform handPivot;
+
+    public GameObject hourHand;
+    public GameObject minuteHand;
+
+    public int speed = 1;
+    void Start()
+    {
+        handPivot = GameObject.Find("Pivot").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            RotateMinute();
+        }
+    }
+    void RotateMinute()
+    {
+
+    }
+    void RotateHour()
+    {
+        hourHand.transform.RotateAround(handPivot.position, Vector3.up, speed);
+        hourHand.transform.RotateAroundLocal(handPivot.position, speed);
+
+
+    }
+}
