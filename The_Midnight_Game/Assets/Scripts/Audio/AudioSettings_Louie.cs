7363f6a60ff7f866aff85a064ede5bee6cfd9bcc
LouieWilliamson <louie-here@hotmail.com>
2020-03-02 17:24:36 +0000

    Implemented Audio Settings Test

---
+++ The_Midnight_Game/Assets/Scripts/Audio/AudioSettings_Louie.cs
@@ -0,0 +1,24 @@
+﻿//Louie Williamson - 02/03/2020
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioSettings_Louie : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetAmbientVolume(float sliderVal)
+    {
+        mixer.SetFloat("AmbientVolume", Mathf.Log10(sliderVal)*20);
+    }
+    public void SetMasterVolume(float sliderVal)
+    {
+        mixer.SetFloat("MasterVolume", Mathf.Log10(sliderVal) * 20);
+    }
+    public void SetSFXVolume(float sliderVal)
+    {
+        mixer.SetFloat("SFXVolume", Mathf.Log10(sliderVal) * 20);
+    }
+}
