5408c9a90deaa644b27e807e0b27bef6aeaac223
Jack Bodicoat <jackbodicoat@gmail.com>
2020-03-06 16:00:12 +0000

    Attic Puzzle Done

---
+++ The_Midnight_Game/Assets/Scripts/AtticPuzzle/LadderAnimation_Jack.cs
@@ -0,0 +1,29 @@
+﻿// Jack 06/03/2020 - Created script
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the ladders animation when falling out the attic.
+/// </summary>
+public class LadderAnimation_Jack : MonoBehaviour
+{
+    public Animator animator;
+
+    /// <summary>
+    /// Enables the attached animator.
+    /// </summary>
+    public void BeginAnimation()
+    {
+        animator.enabled = true;
+    }
+
+    /// <summary>
+    /// Disables the attached animator.
+    /// </summary>
+    public void EndAnimation()
+    {
+        animator.enabled = false;
+    }
+}
