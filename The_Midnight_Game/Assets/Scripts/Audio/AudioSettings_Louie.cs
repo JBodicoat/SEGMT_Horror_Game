0c5293a5a1faf7bda5e90d04d0ed3625f6b67b03
Jack Bodicoat <jackbodicoat@gmail.com>
2020-03-11 15:16:40 +0000

    Review of Audio Settings Done

--- The_Midnight_Game/Assets/Scripts/Audio/AudioSettings_Louie.cs
+++ The_Midnight_Game/Assets/Scripts/Audio/AudioSettings_Louie.cs
@@ -5,24 +5,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+/// <summary>
+/// Description of the class here.
+/// </summary>
 public class AudioSettings_Louie : MonoBehaviour
 {
     public AudioMixer mixer;
     private float volumeLevel;
+    private const float volumeMultiplier = 20;
 
+    /// <summary>
+    /// Description of the function here.
+    /// </summary>
+    /// <param name="sliderVal"></param>
     public void SetAmbientVolume(float sliderVal)
     {
-        volumeLevel = Mathf.Log10(sliderVal) * 20;
+        volumeLevel = Mathf.Log10(sliderVal) * volumeMultiplier;
         mixer.SetFloat("AmbientVolume", volumeLevel);
     }
     public void SetMasterVolume(float sliderVal)
     {
-        volumeLevel = Mathf.Log10(sliderVal) * 20;
+        volumeLevel = Mathf.Log10(sliderVal) * volumeMultiplier;
         mixer.SetFloat("MasterVolume", volumeLevel);
     }
     public void SetSFXVolume(float sliderVal)
     {
-        volumeLevel = Mathf.Log10(sliderVal) * 20;
+        volumeLevel = Mathf.Log10(sliderVal) * volumeMultiplier;
         mixer.SetFloat("SFXVolume", volumeLevel);
     }
     
