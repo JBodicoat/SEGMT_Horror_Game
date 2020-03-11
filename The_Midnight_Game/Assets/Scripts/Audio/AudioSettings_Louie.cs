0b9d6c4907565a7e8a7e50c4da678cc83d0f7f00
Jack Bodicoat <jackbodicoat@gmail.com>
2020-03-11 15:39:49 +0000

    Recorded changes

--- The_Midnight_Game/Assets/Scripts/Audio/AudioSettings_Louie.cs
+++ The_Midnight_Game/Assets/Scripts/Audio/AudioSettings_Louie.cs
@@ -1,4 +1,5 @@
 ﻿//Louie Williamson - 02/03/2020
+// Jack - 11/03/2020 Reviewed. Added placeholder header comments, changed the magic numbers to a constant and cached the strings.
 
 using System.Collections;
 using System.Collections.Generic;
@@ -13,6 +14,9 @@ public class AudioSettings_Louie : MonoBehaviour
     public AudioMixer mixer;
     private float volumeLevel;
     private const float volumeMultiplier = 20;
+    private const string ambientChannel = "AmbientVolume";
+    private const string masterChannel = "MasterVolume";
+    private const string sfxChannel = "SFXVolume";
 
     /// <summary>
     /// Description of the function here.
@@ -21,17 +25,17 @@ public class AudioSettings_Louie : MonoBehaviour
     public void SetAmbientVolume(float sliderVal)
     {
         volumeLevel = Mathf.Log10(sliderVal) * volumeMultiplier;
-        mixer.SetFloat("AmbientVolume", volumeLevel);
+        mixer.SetFloat(ambientChannel, volumeLevel);
     }
     public void SetMasterVolume(float sliderVal)
     {
         volumeLevel = Mathf.Log10(sliderVal) * volumeMultiplier;
-        mixer.SetFloat("MasterVolume", volumeLevel);
+        mixer.SetFloat(masterChannel, volumeLevel);
     }
     public void SetSFXVolume(float sliderVal)
     {
         volumeLevel = Mathf.Log10(sliderVal) * volumeMultiplier;
-        mixer.SetFloat("SFXVolume", volumeLevel);
+        mixer.SetFloat(sfxChannel, volumeLevel);
     }
     
 }
