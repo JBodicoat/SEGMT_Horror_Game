0bb280ea1db181ed0df89ebc2e46cd04ab59ae25
xDanDowning <dandowning96@yahoo.com>
2020-03-18 07:01:13 +0000

    More progress on Clock Puzzle

--- The_Midnight_Game/Assets/Scripts/Clock Puzzle/ClockPuzzle_Dan.cs
+++ The_Midnight_Game/Assets/Scripts/Clock Puzzle/ClockPuzzle_Dan.cs
@@ -5,17 +5,52 @@ using UnityEngine;
 public class ClockPuzzle_Dan : MonoBehaviour
 {
 
-    private Animator ClockPuzzle;
+    // private int gameTime;
+    // private bool clockMove;
+    // private Animator clockPuzzle;
+
+
+    private bool isOpen = false;
 
     void Start()
-    {        
+    {
+
+       // Invoke("ClockDoorMove", 3.0f);
+
+
+        //clockPuzzle = GetComponent<Animator>();
+
         StartCoroutine(ClockSequence());
     }
-    
+
+    void Update()
+    {
+       // if (isOpen)
+       // {
+       //     //Invoke("ClockDoorMove", 3.0f);
+       //     isOpen = false;
+       // }
+
+    }
+
+    void ClockDoorMove()
+    {
+        GameObject.Find("BodyDoor").GetComponent<Animator>().SetBool("isOpen", true);
+    }
+
+    //void ToggleClockDoorMove()
+    //{
+    //    if (isOpen)
+    //    {
+    //        isOpen = false;
+    //    }
+    //}
+
     IEnumerator ClockSequence()
     {
-        yield return new WaitForSeconds(10);
-        ClockPuzzle.Play("Clock Puzzle");
+        Invoke("ClockDoorMove", 3.0f);
+        isOpen = false;
+        yield return new WaitForSeconds(3);
     }
 }
 
